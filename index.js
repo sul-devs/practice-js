9753a432ea71c6ab84d1d936ce3f0d347268abf4
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-02 23:14:40 +0000

    Handle server error.

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-
 // Find an endpoint that has data in it and store it in a variable.
 const url = "https://cat-fact.herokuapp.com/facts";
 
 // Request data from that server.
-const requestedData = fetch(url);
+const requestData = fetch(url);
 
-requestedData.then((dataInResponse) => dataInResponse.json()).then((objects) => objects.map((object) => {
-    const list = document.querySelector("#list");
-    renderData(list, object.text);
-}));
+requestData
+  .then((responseData) => {
+    if (!responseData.ok) {
+      throw new Error(`HTTP error: ${responseData.status}`);
+    } else {
+      return responseData.json();
+    }
+  })
+  .then((objects) =>
+    objects.map((object) => {
+      const list = document.querySelector("#list");
+      renderData(list, object.text);
+    })
+  );
+
+// Render data.
+function renderData(list, text) {
+  const li = document.createElement("li");
+  li.textContent = text;
+  list.appendChild(li);
+}
 
 // Get data.
 // Convert data to JSON.
@@ -20,12 +36,4 @@ requestedData.then((dataInResponse) => dataInResponse.json()).then((objects) => 
 
 // Check if you have access to the data.
 
-
 // Get the data, convert it to JSON, access the parts you need
-
-// Render data.
-function renderData(list, text) {
-    const li = document.createElement("li");
-    li.textContent = text;
-    list.appendChild(li);
-}
--- notes.md
+++ notes.md
@@ -1,4 +1,3 @@
 const list = document.querySelector("#list");
-const url = "https://cat-fact.herokuapp.com/facts";
 const data = fetch(url);
 console.log(data);
