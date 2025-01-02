fe2ae1a62bbc1bc1e511b5228b9d5544f685417f
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-02 18:46:49 +0000

    Render data.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+
 // Find an endpoint that has data in it and store it in a variable.
 const url = "https://cat-fact.herokuapp.com/facts";
 
 // Request data from that server.
 const requestedData = fetch(url);
 
-requestedData.then((dataInResponse) => dataInResponse.json()).then((objects) => objects.map((object) => console.log(object)));
+requestedData.then((dataInResponse) => dataInResponse.json()).then((objects) => objects.map((object) => {
+    const list = document.querySelector("#list");
+    renderData(list, object.text);
+}));
 
 // Get data.
 // Convert data to JSON.
@@ -16,6 +20,12 @@ requestedData.then((dataInResponse) => dataInResponse.json()).then((objects) => 
 
 // Check if you have access to the data.
 
-// Render data.
 
 // Get the data, convert it to JSON, access the parts you need
+
+// Render data.
+function renderData(list, text) {
+    const li = document.createElement("li");
+    li.textContent = text;
+    list.appendChild(li);
+}
