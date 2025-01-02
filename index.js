bbcb650b8e679f619bd53060a31ee3d8ac8d274b
Sulaiman Kamara <sulaimankamara110@gmail.com>
2025-01-02 17:57:33 +0000

    Write comments as instructions for accessing data.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const list = document.querySelector("#list");
-const url = "https://cat-fact.herokuapp.com/facts";
+// Store endpoint in a variable.
 
-// Request data from resource's server.
-const data = fetch(url);
-console.log(data);
+// Access the data at that endpoint. (Calling fetch returns a promise object containing the data at that endpoint)
 
+// Parse the data. (Convert that data from string to json.)
 
-// Convert data to json.
+// Access the specific data you need.
+
+// Handle success in getting data.
+
+// Handle failure in getting data.
 
 // Check if you have access to the data.
 
-// Handle errors in getting data.
-
 // Render data.
+
+// Get the data, convert it to JSON, access the parts you need
